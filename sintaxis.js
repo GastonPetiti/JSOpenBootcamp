ae174e9b9b4cda08cd5e49c5b4a34ff46e341dc1
GastonPetiti <sgastonpetiti@gmail.com>
2022-09-28 23:51:50 -0300

    tarea de sintaxis, variable y palabras reservadas de js

---
+++ sintaxis.js
@@ -0,0 +1,14 @@
+const miNombre = 'Gaston'
+let miEdad = 23
+let fechadeNacimiento = new Date(1999, 8, 14);
+console.log(fechadeNacimiento)
+let libro = {
+    titulo: 'El Principito',
+    autor: 'Antoine de Saint-Exupéry',
+    url:'https://www.casadellibro.com/libro-el-principito/9788478887194/795940',
+    fecha: 2
+}
+
+let dev = (false)
+if (dev === true) console.log('soy dev')
+else console.log('no soy dev, estoy en proceso') 
